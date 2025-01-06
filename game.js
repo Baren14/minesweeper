5bc0ed90375b02732f0a30008bf2d3523da359fc
Baren <tanjiale2015@gmail.com>
2025-01-06 10:20:32 +0800

    modified:   game.js 	modified:   index.html

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
 function start_game(){
-    
+    const counter_x = [0,1,2,3,4,5,6,7,8,9]
+    const counter_y = ['a','b','c','d','e','f','g','h','i','j','k'];
+    const mine_grids = document.getElementById('grids');
+    mine_grids.innerHTML = '';
+
 }
