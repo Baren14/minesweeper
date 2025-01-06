a82b11b8f66a511d86b61d39bbf48c46b343412d
Baren <tanjiale2015@gmail.com>
2025-01-06 10:21:45 +0800

    modified:   game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 function start_game(){
-    const counter_x = [0,1,2,3,4,5,6,7,8,9]
+    const counter_x = ['0','1','2','3','4','5','6','7','8','9']
     const counter_y = ['a','b','c','d','e','f','g','h','i','j','k'];
     const mine_grids = document.getElementById('grids');
     mine_grids.innerHTML = '';
