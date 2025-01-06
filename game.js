c2520d75e610761f445939b996f556dd29609f1d
Baren14 <tanjiale2015@gmail.com>
2025-01-06 10:47:47 +0800

    Update game.js

--- game.js
+++ game.js
@@ -4,9 +4,11 @@ const grid_y = ['a','b','c','d','e','f','g','h','i','j','k'];
 function start_game(){
     const mine_grids = document.getElementById('grids');
     mine_grids.innerHTML = '';
-    grid_y.forEach = () => {
-        grid_x.forEach = () =>{
-            
+    grid_y.forEach(y => {
+        grid_x.forEach(x =>{
+            grid = document.createElement('button');
+            grid.className = 'game_grid';
+            grid.id = y + x;
         }
     }
 }
